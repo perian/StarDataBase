4efab70e7bc51f874422f008331a4fa110fda65f
perian <dmperian@gmail.com>
2022-01-16 17:08:14 +0200

    Настроил получение данных с сервера

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -13,19 +13,72 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResource(`people/`);
-    return res.results;
+
+    return res.results.map(this._transformPerson);;
   }
 
   async getAllStarships() {
     const res = await this.getResource(`starships/`);
-    return res.results;
+    return res.results.map(this._transformStarship);;
   }
 
-  getPerson(id) {
-    return this.getResource(`people/${id}/`);
+  async getAllPlanets() {
+    const res = await this.getResource(`planets/`);
+    console.log(this._transformPlanet)
+    return res.results.map(this._transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`planets/${id}/`);
+  async getPerson(id) {
+    const people = this.getResource(`people/${id}/`);
+    return this._transformPerson(people);
+  }
+
+  async getStarship(id) {
+    const starship = this.getResource(`starships/${id}/`);
+    return this._transformStarship(starship);
+  }
+
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    }
+  }
+
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    }
+  }
+  
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    };
   }
 };
